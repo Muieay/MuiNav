a60f8949350d0b3c142a455e79489d6ea30532c2
Muieay <muieay@qq.com>
2024-06-09 00:56:10 +0800

    代码加密混淆配置

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,33 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+const WebpackObfuscator = require('webpack-obfuscator');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = defineConfig({
-  transpileDependencies: true,
-  chainWebpack: config => {
-    config
-        .plugin('html')
-        .tap(args => {
-          args[0].title= 'Muieay^网址导航'
-          return args
-        })
-  }
+    transpileDependencies: true,
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'Muieay^网址导航'
+                return args
+            })
+    },
+    productionSourceMap: false,
+    configureWebpack: (config) => {
+        if (process.env.NODE_ENV === 'production') {
+            // 启用Webpack Obfuscator插件进行代码加密
+            config.plugins.push(
+                new WebpackObfuscator({
+                    rotateUnicodeArray: true, // 打乱Unicode数组顺序
+                })
+            );
+            // 启用Terser插件进行代码混淆
+            config.optimization.minimizer.push(
+                new TerserPlugin({
+                    terserOptions: {
+                        mangle: true, // 开启变量名混淆
+                    },
+                })
+            );
+        }
+    },
 })
